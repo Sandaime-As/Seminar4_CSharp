ce620aea4baf549884abcd8fa611a1c652ae6689
Sandaime-As <asenov1998@mail.ru>
2022-10-15 23:23:41 +0600

    add task26

---
+++ task26/Program.cs
@@ -0,0 +1,28 @@
+﻿/* **Задача 26:**
+
+Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+456 -> 3
+
+78 -> 2
+
+89126 -> 5 */
+
+using static System.Console;
+Clear();
+Write("Enter the number: ");
+
+int Sum = int.Parse(ReadLine()!);
+int sum = GetSum(Sum);
+WriteLine($"Количество цифр в числе: {sum}");
+
+int GetSum(int number)
+{
+    int result=0;
+    while(number>0)         // через while
+    {
+        number/=10;
+        result++;
+    }
+    return result;
+}
